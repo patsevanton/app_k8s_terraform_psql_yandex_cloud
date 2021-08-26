66fdddb16778bba11bfe8d204ea16bd44be7aad6
Anton Patsev <patsev.anton@gmail.com>
2021-08-26 16:23:55 +0600

    add yandex_kubernetes_node_group

--- terraform-k8s/k8s.tf
+++ terraform-k8s/k8s.tf
@@ -1,10 +1,12 @@
+# yandex_kubernetes_cluster
+
 resource "yandex_kubernetes_cluster" "zonal_cluster_resource_name" {
   name        = "my-cluster"
   description = "my-cluster description"
   network_id = "${yandex_vpc_network.this.id}"
 
   master {
-    version = "1.17"
+    version = "1.18"
     zonal {
       zone      = "${yandex_vpc_subnet.subnet_resource_name.zone}"
       subnet_id = "${yandex_vpc_subnet.subnet_resource_name.id}"
@@ -15,14 +17,94 @@ resource "yandex_kubernetes_cluster" "zonal_cluster_resource_name" {
   service_account_id      = "${yandex_iam_service_account.this.id}"
   node_service_account_id = "${yandex_iam_service_account.this.id}"
   release_channel = "STABLE"
-  depends_on = ["yandex_resourcemanager_folder_iam_member.this"]
+  depends_on = [yandex_resourcemanager_folder_iam_member.this]
+}
+
+# yandex_kubernetes_node_group
+
+resource "yandex_kubernetes_node_group" "my_node_group" {
+  cluster_id  = "${yandex_kubernetes_cluster.zonal_cluster_resource_name.id}"
+  name        = "name"
+  description = "description"
+  version     = "1.18"
+
+  labels = {
+    "key" = "value"
+  }
+
+  instance_template {
+    platform_id = "standard-v2"
+
+    network_interface {
+      nat                = true
+      subnet_ids         = ["${yandex_vpc_subnet.subnet_resource_name.id}"]
+      security_group_ids = ["${yandex_vpc_security_group.my_sg.id}"]
+    }
+
+    resources {
+      memory = 2
+      cores  = 2
+    }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 32
+    }
+
+    scheduling_policy {
+      preemptible = false
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 1
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = "ru-central1-c"
+    }
+  }
+
+  maintenance_policy {
+    auto_upgrade = true
+    auto_repair  = true
+
+    maintenance_window {
+      day        = "monday"
+      start_time = "15:00"
+      duration   = "3h"
+    }
+
+    maintenance_window {
+      day        = "friday"
+      start_time = "10:00"
+      duration   = "4h30m"
+    }
+  }
+}
+
+resource "yandex_vpc_security_group" "my_sg" {
+  name           = "My security group"
+  description    = "description for my security group"
+  network_id     = yandex_vpc_network.this.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "ingress"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = "6432"
+  }
+
 }
 
 resource "yandex_vpc_network" "this" {}
 
 resource "yandex_vpc_subnet" "subnet_resource_name" {
   network_id     = yandex_vpc_network.this.id
-  zone = "ru-central1-a"
+  zone = "ru-central1-c"
   v4_cidr_blocks = ["192.168.20.0/24"]
 }
 
@@ -31,7 +113,7 @@ resource "yandex_iam_service_account" "this" {
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "this" {
-  folder_id = var.folder-id
+  folder_id = var.yc_folder_id
 
   member = "serviceAccount:${yandex_iam_service_account.this.id}"
   role   = "editor"
